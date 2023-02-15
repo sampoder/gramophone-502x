f402ff768f63191a4781be0bb3457c7ed783ba11
Sam Poder <39828164+sampoder@users.noreply.github.com>
2023-02-15 21:57:57 +0800

    Revert "call back"
    This reverts commit 30f1505d1a7ef63015e78b688344f3e906a870e3.

--- web-server/pages/api/devices.js
+++ web-server/pages/api/devices.js
@@ -14,23 +14,15 @@ export default (req, res) => {
 	spotifyApi.refreshAccessToken().then(
 		function(data) {
 			console.log('The access token has been refreshed!');
-			spotifyApi.setAccessToken(data.body['access_token']).then(
-				function() {
-					spotifyApi.getMyDevices().then(
-						function(data) {
-							console.log('The device list is here!');
-							res.json(data);
-						},
-						function(err) {
-							console.log(err);
-							console.log('Device list failed.');
-							res.send('🐛');
-						}
-					);
+			spotifyApi.setAccessToken(data.body['access_token']);
+			spotifyApi.getMyDevices().then(
+				function(data) {
+					console.log('The device list is here!');
+					res.json(data);
 				},
 				function(err) {
 					console.log(err);
-					console.log('Auth failed.');
+					console.log('Device list failed.');
 					res.send('🐛');
 				}
 			);
