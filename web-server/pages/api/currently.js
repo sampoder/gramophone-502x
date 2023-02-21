1d60d667cb0ac8e70c4815d92d2a4d78b59f9071
Sam Poder <sam.r.poder@gmail.com>
2023-02-22 00:07:57 +0800

    Create currently.js

---
+++ web-server/pages/api/currently.js
@@ -0,0 +1,43 @@
+var SpotifyWebApi = require('spotify-web-api-node');
+
+export default async (req, res) => {
+	var clientId = process.env.SPOTIFY_CLIENT_ID,
+		clientSecret = process.env.SPOTIFY_CLIENT_SECRET,
+		refreshToken = process.env.SPOTIFY_REFRESH_TOKEN;
+
+	var spotifyApi = new SpotifyWebApi({
+		clientId: clientId,
+		clientSecret: clientSecret,
+		refreshToken: refreshToken,
+	});
+
+	await spotifyApi.refreshAccessToken().then(
+		function(data) {
+			console.log('The access token has been refreshed!');
+			spotifyApi.setAccessToken(data.body['access_token']);
+		},
+		function(err) {
+			console.log(err);
+			console.log('Auth failed.');
+			return res.send('🐛');
+		}
+	);
+
+	await spotifyApi.getArtistAlbums('43ZHCT0cAZBISjO8DG9PnE').then(
+		function(data) {
+			console.log('Artist albums', data.body);
+		},
+		function(err) {
+			console.error(err);
+		}
+	);
+  
+  spotifyApi.getMyCurrentPlayingTrack()
+  .then(function(data) {
+    res.send(data.body.item);
+  }, function(err) {
+    console.log(err);
+			console.log('Device list failed.');
+			res.send('🐛');
+  });
+};
